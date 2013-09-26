7943529a586749420fce9a8d8db303fcfc0f5267
Sam Olsen <samolsen@Sams-iMac.local>
2013-09-26 12:42:00 -0700

    Initial Commit

---
+++ WebViewMemoryLeak/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WebViewMemoryLeak
+//
+//  Created by Sam Olsen on 9/26/13.
+//  Copyright (c) 2013 Sam Olsen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
